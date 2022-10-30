60e3f01353fc1a6b9058ccabb34b0cfb8d178f80
Ying Ying <yingying@KuangYesMacBook.attlocal.net>
2022-10-29 20:33:21 -0700

    create first prompt

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
 // Import and require mysql2
 const mysql = require('mysql2');
+
+const inquirer = require('inquirer');
+const { default: Choices } = require('inquirer/lib/objects/choices');
+
+function companyStructure() = {
+    inquirer.prompt ([
+        {
+            type:"list",
+            name: "overview",
+            message: "Please select the following option.",
+            choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role", "Exit"],
+        }
+    ])
+    .then(answers => {
+        const {menu} = answers;
+
+        switch (menu) {
+            case 'View all departments':
+                break;
+            case 'View all roles':
+                break;
+            case 'View all employees':
+                break;
+            case 'Add a department':
+                break;
+            case 'Add an employee':
+                break;
+            case 'Update an employee role':
+                break;
+            case 'Exit':
+                break;
+        }
+    })
+}
